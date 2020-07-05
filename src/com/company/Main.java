dffdf3b770a6d8137c95aa01f9d4c1dde78cee28
4ecHok <888>
2020-07-05 13:54:32 +0300

    make some reworkings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,6 @@ public class Main {
         int a = getFoot();
         System.out.println("Введите основание целевой системы счисления");
         int b = getFoot();
-        //тут считываем число
         System.out.println("Введите число, записанное в а-ричной системе счисления");
         String x = getNum(a);
         System.out.println("Число " + x + " в " + a + "-ричной системе счисления ");
@@ -43,7 +42,7 @@ public class Main {
         boolean good;
         do {
             Scanner scan = new Scanner(System.in);
-            HashMap<String, String> map = mapForCalc(a);
+            HashMap<String, String> map = mapForCalc(true, a);
 
             if (scan.hasNextLine()) {
                 boolean flag = true;
@@ -74,7 +73,7 @@ public class Main {
         if (a == b) {
             answer = new StringBuilder(x);
         } else {
-            map = mapForCalc(b);
+            map = mapForCalc(true, b);
             int transformTo10 = transformFromATo10(x, a);
 
             do {
@@ -88,7 +87,19 @@ public class Main {
         return answer.toString();
     }
 
-    static HashMap<String, String> mapForCalc( int b) {
+    static int transformFromATo10(String x, int a) {
+        int transform = 0;
+        HashMap<String, String> map = mapForCalc(false, a);
+
+        for (int i = 0; i < x.length(); i++) {
+            String numChar = String.valueOf(x.charAt(i));
+            int mult = Integer.parseInt(map.get(numChar));
+            transform += mult * Math.pow(a, (x.length() - 1) - i);
+        }
+        return transform;
+    }
+
+    static HashMap<String, String> mapForCalc(boolean isReversed, int b) {
         HashMap<String, String> map = new HashMap<>();
         String [] arrayForEntering = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
                 "t","u","v","w","x","y","z"};
@@ -101,42 +112,19 @@ public class Main {
             for (int i = 0; i < 9; i++) {
                 map.put(String.valueOf(i), String.valueOf(i));
             }
-            for (int i = 0; i < b - 10; i++) {
-                map.put(String.valueOf(i + 10), arrayForEntering[i]);
+            if (isReversed) {
+                for (int i = 0; i < b - 10; i++) {
+                    map.put(String.valueOf(i + 10), arrayForEntering[i]);
+                }
+            } else {
+                for (int i = 0; i < b - 10; i++) {
+                    map.put(arrayForEntering[i], String.valueOf(i + 10));
+                }
             }
         }
 
         return map;
     }
-
-    static int transformFromATo10(String x, int a) {
-        int transform = 0;
-
-        HashMap<String, String> map = new HashMap<>();
-        String [] arrayForEntering = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
-                "t","u","v","w","x","y","z"};
-
-        if (a < 10) {
-            for (int i = 0; i < a; i++) {
-                map.put(String.valueOf(i), String.valueOf(i));
-            }
-        } else {
-            for (int i = 0; i < 9; i++) {
-                map.put(String.valueOf(i), String.valueOf(i));
-            }
-            for (int i = 0; i < a - 10; i++) {
-                map.put(arrayForEntering[i], String.valueOf(i + 10));
-            }
-        }
-
-        for (int i = 0; i < x.length(); i++) {
-            String numChar = String.valueOf(x.charAt(i));
-            int mult = Integer.parseInt(map.get(numChar));
-            transform += mult * Math.pow(a, (x.length() - 1) - i);
-        }
-        return transform;
-    }
-
 
 
 }
